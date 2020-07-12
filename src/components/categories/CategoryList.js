90b42a3eab48ed73f86d7a3016e11c945c0364f1
unknown <grknzr2609@gmail.com>
2020-07-12 13:54:46 +0300

    json dan kategoriler çekildi

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
-
-export default class CaegoryList extends Component {
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import * as categoryActions from '../../redux/actions/categoryActions'
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+class CategoryList extends Component {
+    componentDidMount() {
+        this.props.actions.getCategories();
+    }
+    selectCategory=(category)=>{
+        this.props.actions.changeCategory(category);
+    }
     render() {
         return (
             <div>
-                <h3>Categories</h3>
+                <h3>Categories</h3>  
+                    <ListGroup>
+                        {
+                            this.props.categories.map(category=>(
+                                <ListGroupItem 
+                                active={category.id===this.props.currentCategory.id}
+                                onClick={()=>this.selectCategory(category)} key={category.id}>{category.categoryName}</ListGroupItem>
+                            ))
+                        }
+                    </ListGroup>
             </div>
         )
     }
 }
+function mapStateToProps(state){
+    return {
+        currentCategory:state.changeCategoryReducer,
+        categories:state.categoryListReducer
+    }
+}
+function mapDispatchToProps(dispatch){
+    return {
+        actions:{
+            getCategories:bindActionCreators(categoryActions.getCategories,dispatch),
+            changeCategory:bindActionCreators(categoryActions.changeCategory,dispatch)
+        }
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(CategoryList)
--- src/components/products/ProductList.js
+++ src/components/products/ProductList.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 
-export default class ProductList extends Component {
+class ProductList extends Component {
     render() {
         return (
             <div>
-                <h3>Products</h3>
+                <h3>Products {this.props.currentCategory.categoryName}</h3>
             </div>
         )
     }
 }
+
+function mapStateToProps(state){
+    return {
+        currentCategory:state.changeCategoryReducer
+    }
+}
+
+export default connect(mapStateToProps)(ProductList)
